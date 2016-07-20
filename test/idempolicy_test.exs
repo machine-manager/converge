a38055cb09741bf0f36752e9b0605bf2f7f61c4f
Ivan Kozik <ivan@ludios.org>
2016-07-20 11:12:40 +0000

    Add test: Idempolicy.converge doesn't call meet() if met? returns true

--- test/idempolicy_test.exs
+++ test/idempolicy_test.exs
@@ -11,6 +11,22 @@ defimpl Converge, for: FailsToConvergePolicy do
 	end
 end
 
+defmodule AlreadyConvergedPolicy do
+	defstruct []
+end
+
+defimpl Converge, for: AlreadyConvergedPolicy do
+	import ExUnit.Assertions, only: [assert: 1, assert: 2]
+
+	def met?(_) do
+		true
+	end
+
+	def meet(_) do
+		assert false, "unreachable"
+	end
+end
+
 defmodule IdempolicyTest do
 	use ExUnit.Case
 	doctest Idempolicy
@@ -19,5 +35,11 @@ defmodule IdempolicyTest do
 		ftc = %FailsToConvergePolicy{}
 		rep = Reporter
 		assert_raise(ConvergeError, ~r/Failed to converge: /, fn -> Idempolicy.converge(ftc, rep) end)
+	end
+
+	test "Idempolicy.converge doesn't call meet() if met? returns true" do
+		acp = %AlreadyConvergedPolicy{}
+		rep = Reporter
+		Idempolicy.converge(acp, rep)
 	end
 end
