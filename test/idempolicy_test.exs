a793a5746cc0740cd6ee170fc65ccb4275df77cf
Ivan Kozik <ivan@ludios.org>
2016-07-20 11:59:51 +0000

    Test that met? is called after meet

--- test/idempolicy_test.exs
+++ test/idempolicy_test.exs
@@ -1,3 +1,5 @@
+import ExUnit.Assertions, only: [assert: 1, assert: 2]
+
 defmodule FailsToConvergePolicy do
 	defstruct []
 end
@@ -11,13 +13,12 @@ defimpl Converge, for: FailsToConvergePolicy do
 	end
 end
 
+
 defmodule AlreadyConvergedPolicy do
 	defstruct []
 end
 
 defimpl Converge, for: AlreadyConvergedPolicy do
-	import ExUnit.Assertions, only: [assert: 1, assert: 2]
-
 	def met?(_) do
 		true
 	end
@@ -26,6 +27,31 @@ defimpl Converge, for: AlreadyConvergedPolicy do
 		assert false, "unreachable"
 	end
 end
+
+
+defmodule ConvergeablePolicy do
+	defstruct pid: nil
+
+	def new do
+		{:ok, pid} = Agent.start_link(fn -> {false, 0} end)
+		%ConvergeablePolicy{pid: pid}
+	end
+
+	def get_met_count(p) do
+		Agent.get(p.pid, fn({has_met, met_count}) -> met_count end)
+	end
+end
+
+defimpl Converge, for: ConvergeablePolicy do
+	def met?(p) do
+		Agent.get_and_update(p.pid, fn({has_met, met_count}) -> {has_met, {has_met, met_count + 1}} end)
+	end
+
+	def meet(p) do
+		Agent.update(p.pid, fn({has_met, met_count}) -> {true, met_count} end)
+	end
+end
+
 
 defmodule SilentReporter do
 	def running(_) do end
@@ -47,5 +73,12 @@ defmodule IdempolicyTest do
 		acp = %AlreadyConvergedPolicy{}
 		rep = SilentReporter
 		Idempolicy.converge(acp, rep)
+	end
+
+	test "Idempolicy.converge calls met? again after calling meet" do
+		cp = ConvergeablePolicy.new
+		rep = SilentReporter
+		Idempolicy.converge(cp, rep)
+		assert ConvergeablePolicy.get_met_count(cp) == 2
 	end
 end
