4971526cf0625bcf8ef22105cf286f71a2d6e5e0
Ivan Kozik <ivan@ludios.org>
2016-10-05 08:21:28 +0000

    Add /etc/passwd parser

--- lib/user.ex
+++ lib/user.ex
@@ -1,4 +1,21 @@
-defmodule UserExists do
+defmodule Converge.UserUtil do
+	defp line_to_tuple(line) do
+		[name, "x", uid_s, gid_s, comment, home, shell] = String.split(line, ":")
+		{uid, ""} = Integer.parse(uid_s)
+		{gid, ""} = Integer.parse(gid_s)
+		{name, %{uid: uid, gid: gid, comment: comment, home: home, shell: shell}}
+	end
+
+	def get_users() do
+		File.read!("/etc/passwd")
+			|> String.trim_trailing("\n")
+			|> String.split("\n")
+			|> Enum.map(&line_to_tuple/1)
+			|> Enum.into(%{})
+	end
+end
+
+defmodule Converge.UserExists do
 	@enforce_keys [:name]
 	defstruct name: nil
 end
