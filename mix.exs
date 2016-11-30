2b8ec2be6d7540fdabf9155ec2cc793c766a4062
Ivan Kozik <ivan@ludios.org>
2016-11-30 14:11:11 +0000

    Require elixir >= 1.4.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Converge.Mixfile do
 		[
 			app: :converge,
 			version: "0.1.0",
-			elixir: ">= 1.3.2",
+			elixir: ">= 1.4.0",
 			build_embedded: Mix.env == :prod,
 			start_permanent: Mix.env == :prod,
 			# Keep this disabled for tests because our tests define some Unit
