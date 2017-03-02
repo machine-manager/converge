29584aa1281efca19461543df627d0f6397ba039
Ivan Kozik <ivan@ludios.org>
2017-03-02 23:31:19 +0000

    Add SystemdUnitEnabled, SystemdUnitDisabled

--- lib/systemd.ex
+++ lib/systemd.ex
@@ -1,4 +1,4 @@
-alias Converge.Unit
+alias Converge.{Unit, UnitError}
 
 defmodule Converge.SystemdUnitStarted do
 	@moduledoc """
@@ -38,5 +38,66 @@ defimpl Unit, for: Converge.SystemdUnitStopped do
 
 	def meet(u, _ctx) do
 		{"", 0} = System.cmd("systemctl", ["stop", "--", u.name])
+	end
+end
+
+
+defmodule Converge.SystemdUnitEnabled do
+	@moduledoc """
+	Systemd unit `name` is enabled.
+	"""
+	@enforce_keys [:name]
+	defstruct name: nil
+end
+
+defimpl Unit, for: Converge.SystemdUnitEnabled do
+	def met?(u, _ctx) do
+		# Exit code 0 if enabled, 1 if disabled or missing
+		{out, code} = System.cmd("systemctl", ["is-enabled", "--", u.name], stderr_to_stdout: true)
+		# Instead of just "enabled\n" or "disabled\n", we can also get output like:
+		# """
+		# chrony.service is not a native service, redirecting to systemd-sysv-install
+		# Executing /lib/systemd/systemd-sysv-install is-enabled chrony
+		# enabled
+		# """
+		case {code, out |> get_last_line} do
+			{0, "enabled"}  -> true
+			{1, "disabled"} -> false
+			_               -> raise UnitError, message: """
+			                                             Failed to get enabled/disabled status for #{inspect u.name}; \
+			                                             did you run `systemctl daemon-reload` first?
+			                                             {exit_code, stdout_and_stderr}: #{inspect {code, out}}
+			                                             """
+		end
+	end
+
+	def meet(u, _ctx) do
+		{_, 0} = System.cmd("systemctl", ["enable", "--", u.name], stderr_to_stdout: true)
+	end
+
+	defp get_last_line(s) do
+		s
+		|> String.trim
+		|> String.split("\n")
+		|> List.last
+	end
+end
+
+
+defmodule Converge.SystemdUnitDisabled do
+	@moduledoc """
+	Systemd unit `name` is disabled.
+	"""
+	@enforce_keys [:name]
+	defstruct name: nil
+end
+
+defimpl Unit, for: Converge.SystemdUnitDisabled do
+	def met?(u, ctx) do
+		not Converge.Unit.Converge.SystemdUnitEnabled.met?(u, ctx)
+	end
+
+	def meet(u, _ctx) do
+		{_, 0} = System.cmd("systemctl", ["disable", "--", u.name], stderr_to_stdout: true)
 	end
 end
--- test/systemd_test.exs
+++ test/systemd_test.exs
@@ -1,11 +1,11 @@
-alias Converge.{Runner, SystemdUnitStarted, SystemdUnitStopped}
+alias Converge.{Runner, SystemdUnitStarted, SystemdUnitStopped, SystemdUnitEnabled, SystemdUnitDisabled}
 alias Converge.TestHelpers.{TestingContext}
 
 defmodule Converge.Runner.SystemdUnitStartedTest do
 	use ExUnit.Case
 
 	test "SystemdUnitStarted" do
-		u = %SystemdUnitStarted{name: "chrony"}
+		u = %SystemdUnitStarted{name: "chrony.service"}
 		Runner.converge(u, TestingContext.get_context())
 	end
 end
@@ -14,7 +14,27 @@ defmodule Converge.Runner.SystemdUnitStoppedTest do
 	use ExUnit.Case
 
 	test "SystemdUnitStopped" do
-		u = %SystemdUnitStopped{name: "chrony"}
+		u = %SystemdUnitStopped{name: "chrony.service"}
+		Runner.converge(u, TestingContext.get_context())
+	end
+end
+
+
+defmodule Converge.Runner.SystemdUnitEnabled do
+	use ExUnit.Case
+
+	test "SystemdUnitEnabled" do
+		u = %SystemdUnitEnabled{name: "chrony.service"}
+		Runner.converge(u, TestingContext.get_context())
+	end
+end
+
+
+defmodule Converge.Runner.SystemdUnitDisabled do
+	use ExUnit.Case
+
+	test "SystemdUnitDisabled" do
+		u = %SystemdUnitDisabled{name: "chrony.service"}
 		Runner.converge(u, TestingContext.get_context())
 	end
 end
