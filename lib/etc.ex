2076efaa816905737bcbdcfc150847f36f103249
Ivan Kozik <ivan@ludios.org>
2017-10-29 18:46:09 +0000

    EtcCommitted: install prerequisite git and etckeeper packages as needed

--- lib/etc.ex
+++ lib/etc.ex
@@ -1,4 +1,4 @@
-alias Converge.Unit
+alias Converge.{Unit, Util}
 
 defmodule Converge.EtcCommitted do
 	@moduledoc """
@@ -9,16 +9,30 @@ end
 
 defimpl Unit, for: Converge.EtcCommitted do
 	def met?(_u, _ctx) do
+		maybe_install_prerequisites()
 		{out, 0} = System.cmd("git", ["--work-tree=/etc", "--git-dir=/etc/.git", "status", "--short"])
 		String.trim_trailing(out) == ""
 	end
 
 	def meet(u, _ctx) do
+		maybe_install_prerequisites()
 		message = case u.message do
 			nil -> "converge"
 			s   -> s
 		end
 		# Note: "--" argument after "commit" is not needed or allowed here
 		{_, 0} = System.cmd("etckeeper", ["commit", message])
+	end
+
+	defp maybe_install_prerequisites() do
+		unless File.exists?("/usr/bin/etckeeper") and File.exists?("/usr/bin/git") do
+			Util.update_package_index()
+		end
+		unless File.exists?("/usr/bin/git") do
+			Util.install_package("git")
+		end
+		unless File.exists?("/usr/bin/etckeeper") do
+			Util.install_package("etckeeper")
+		end
 	end
 end
